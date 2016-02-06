fb89d096dc2cc45488403aa2fa6c8942ccfdad9e
Nadia Pillard <ua9ond@yandex.ru>
2016-02-06 19:31:10 +0600

    animation added

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,10 @@
 var menuElem = document.querySelector(".main_nav");
 window.onscroll = function() {
 	if (window.pageYOffset > 250) {
-		menuElem.classList.add("popup_show");
+		menuElem.classList.add("menu_show");
 	};
 	if (window.pageYOffset < 250) {
-		if (menuElem.classList.contains("popup_show"))
-		menuElem.classList.remove("popup_show");
+		if (menuElem.classList.contains("menu_show"))
+		menuElem.classList.remove("menu_show");
 	};
 };
