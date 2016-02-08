828f99eed25f255c1d17ff4652c4a1b5f36bdc0b
Nadia Pillard <ua9ond@yandex.ru>
2016-02-08 22:41:40 +0600

    mobile popup and menu animation

---
+++ js/menu_moblie.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+	var mobileMenuLink = $("div.mobile_menu");
+	var mobileMenu = $(".mobile_menu_popup");
+	var mobileMenuClose = $(".menu_close");
+	var mobileSubscribeLink = $(".mobile_subscribe_link");
+	var mobileEmailLink = $(".mobile_email_link");
+	
+	mobileMenuLink.click ( function() {
+		mobileMenu.css('display','block').animate({'top': '0'}, 600);	
+	});
+	
+	
+	mobileMenuClose.click( function() {
+	mobileMenu.animate({top: '-500'}, 400,
+					   function(){
+								$(this).css('display','none');
+							}
+						  );
+	});
+	
+	mobileSubscribeLink.click( function() {
+	mobileMenu.css('display','none');
+					   
+	});
+	
+	mobileEmailLink.click( function() {
+	mobileMenu.css('display','none');
+	});
+	
+										 
+});
+	
+					
+				  
+				  
