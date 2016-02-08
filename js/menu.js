c6dd7272aeaf3f95a55940b3bc36e11d773b2620
Nadia Pillard <ua9ond@yandex.ru>
2016-02-08 19:27:23 +0600

    menu animation added

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,29 @@
 $(function() {
 	$(window).scroll(function() {
-		if ($(this).scrollTop() > 200) {
-			  if ($(window).width() <= 490) {
+		if ($(window).width() <= 490) {
+			if ($(this).scrollTop() > 100) {
 				$("nav.mobile_nav").animate({'top':'0px'},400);
-			  };
+			}
+			else {
+				$("nav.mobile_nav").stop(true).animate({'top':'-105px'},300);
+			}
 		}
-				else{
-					$("nav.mobile_nav").stop(true).animate({'top':'-105px'},300);
-		};
-
+		else {
+			if ($(this).scrollTop() > 200) {
+				$("nav.main_nav").animate({'top':'0px'},400);
+			}
+			else {
+				$("nav.main_nav").stop(true).animate({'top':'-105px'},300);
+			}
+		}
 	});
 });
+		
+		
+		
+		
+		
+		
+		
+		
+
